de47b0dd5e222ef11d2a7912fde5de090ec68c65
SureshbabuKonapuram <suresh.konapuram@nstarxinc.com>
2024-04-23 17:48:16 +0530

    db connection

--- app.js
+++ app.js
@@ -11,7 +11,19 @@ const mongoose=require('mongoose')
 //Middleware
 app.use(express.json());
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
+
+
+const productSchema=mongoose.Schema({
+    name:String,
+    image:String,
+    countInStock:{
+        type:Number,
+        required:true
+    }
+});
+
+const Product=mongoose.model('Product',productSchema)
 
 mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
     console.log("data base connection is ready")
@@ -19,22 +31,32 @@ mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
     console.log(err)
 })
 
-app.get(`${api}+/products`,(req,res)=>{
-    const product={
-        id:1,
-        name:"hair dresser",
-        image:'some_url'
+app.get(`${api}+/products`,async(req,res)=>{
+    const productList=await Product.find();
+    if(!productList){
+        res.status(500).json({success:false})
     }
-    res.send(product)
+    res.send(productList)
 })
 
 app.post(`${api}+/products`,(req,res)=>{
-    const newProduct=req.body;
-    console.log(newProduct)
-    res.send(newProduct)
+   const product=new Product({
+    name:req.body.name,
+    image:req.body.image,
+    countInStock:req.body.countInStock
+   })
+   product.save().then((createdProduct=>{
+    res.status(201).json(createdProduct)
+   })).catch((err)=>{
+    res.status(500).json({
+        error:err,
+        success:false
+    })
+   })
 })
 
-app.listen(3001,()=>{
+
+app.listen(3000,()=>{
     console.log(api);
     console.log("server is running now on https://localhost:3001")
 })
