f6c316caa1e8adcf93faace1477d54cdc81475af
SureshbabuKonapuram <suresh.konapuram@nstarxinc.com>
2024-04-24 18:02:20 +0530

    products api modifications

--- routes/products.js
+++ routes/products.js
@@ -1,3 +1,5 @@
+
+const { Category } = require('../models/category');
 const {Product}=require('../models/product')
 const express=require('express');
 const router =express.Router();
@@ -7,23 +9,41 @@ router.get(`/`,async(req,res)=>{
     if(!productList){
         res.status(500).json({success:false})
     }
-    res.send(productList)
+    res.send(productList);
 })
 
-router.post(`/`,(req,res)=>{
-   const product=new Product({
+router.get(`/:id`,async(req,res)=>{
+    const product=await Product.findById(req.params.id);
+    if(!product){
+        res.status(500).json({success:false})
+    }
+    res.send(product);
+})
+
+router.post(`/`,async(req,res)=>{
+   let product=new Product({
     name:req.body.name,
+    description:req.body.description,
+    richDescription:req.body.richDescription,
     image:req.body.image,
-    countInStock:req.body.countInStock
-   })
-   product.save().then((createdProduct=>{
-    res.status(201).json(createdProduct)
-   })).catch((err)=>{
-    res.status(500).json({
-        error:err,
-        success:false
-    })
-   })
+    brand:req.body.brand,
+    price:req.body.price,
+    category:req.body.category,
+    countInStock:req.body.countInStock,
+    rating:req.body.rating,
+    numReviews:req.body.numReviews,
+    isFeatured:req.body.isFeatured,
+
+   });
+   const category =await Category.findById(req.body.category);
+    if(!category){
+        res.status(400).send("Invalid category")
+    }
+   product=await product.save();
+   if(!product){
+    return res.status(500).send('The Product is Not Created')
+   }
+   res.send(product);
 })
 
 module.exports=router;
